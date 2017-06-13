31156bd8b49a161ea18bc760d96c5f5256642a87
singhsonu778 <singhsonu778@gmail.com>
2017-06-13 19:41:31 +0530

    Add todos reducer

---
+++ src/js/reducer/todos.js
@@ -0,0 +1,25 @@
+const todos = (state = [], action) => {
+   switch (action.type) {
+      case 'ADD_TODO':
+         return [
+            ...state, {
+               id: action.id,
+               text: action.text,
+               completed: false
+            }
+         ];
+      case 'TOGGLE_TODO':
+         return state.map(todo => {
+            if (todo.id !== action.id) {
+               return todo;
+            } else {
+               return {
+                  ...todo,
+                  completed: !todo.completed
+               };
+            }
+         });
+      default:
+         return state;
+   }
+};
