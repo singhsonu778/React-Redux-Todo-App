f475ea52cea231cbc302221d594131b4401544fe
singhsonu778 <singhsonu778@gmail.com>
2017-06-13 20:57:30 +0530

    Create store using reducers

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import '../scss/index.scss';
+import reducers from './reducer/reducers.js';
+
+const store = createStore(reducers);
 
 ReactDOM.render(
 	<h1>React Redux ToDo App</h1>,
