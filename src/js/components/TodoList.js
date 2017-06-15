ef2df3703b85f4ed83eec8d625a3d6a152f15ab9
singhsonu778 <singhsonu778@gmail.com>
2017-06-15 23:26:25 +0530

    Extract onClickHandler to separate function

--- src/js/components/TodoList.js
+++ src/js/components/TodoList.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 import Todo from './Todo';
 
 class TodoList extends Component {
+
+   handleClick(todo) {
+		console.log('Todolist...handleClick method...');
+		this.props.onClickHandler(todo.id);
+   }
+
    render() {
       return (
 		  <ul>
 		    {
 				this.props.todos.map(todo =>
-					<Todo key={todo.id} text={todo.text} completed={todo.completed} onClickHandler={() => {
-							console.log('...todolist....onclick method....');
-							this.props.onClickHandler(todo.id);
-						}
-					}/>
+					<Todo key={todo.id} text={todo.text} completed={todo.completed} onClickHandler={this.handleClick.bind(this, todo)}/>
 				)
 		    }
 		  </ul>
